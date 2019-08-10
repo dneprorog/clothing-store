58d8b9e8fdb2e1e6ed93fa5067a62514c89be5a5
max.prikhodko <maksimprikhodkodnepr@gmail.com>
2019-08-10 20:41:17 +0300

    master   - install react-router-dom   - created new page HatsPage   - update application

--- src/components/directory/directory.component.jsx
+++ src/components/directory/directory.component.jsx
@@ -10,7 +10,7 @@ class Directory extends Component {
                     title: 'hats',
                     imageUrl: 'https://i.ibb.co/cvpntL1/hats.png',
                     id: 1,
-                    linkUrl: 'shop/hats'
+                    linkUrl: 'hats'
                 },
                 {
                     title: 'jackets',
@@ -45,12 +45,10 @@ class Directory extends Component {
         return(
             <div className='directory-menu'>
                 {
-                    this.state.sections.map(({title, imageUrl, id, size}) => (
+                    this.state.sections.map(({id, ...otherSectionProps}) => (
                        <MenuItem
                            key={id}
-                           title={title}
-                           imageUrl={imageUrl}
-                           size={size}
+                           {...otherSectionProps}
                        />
                     ))
                 }
